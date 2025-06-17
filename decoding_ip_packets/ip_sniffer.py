e437d4530f4aedac8183087b552af0996473e041
kotkonsolniy <kotkonsolniy@gmail.com>
2025-06-17 08:50:24 +0300

    alles

--- decoding_ip_packets/ip_sniffer.py
+++ decoding_ip_packets/ip_sniffer.py
@@ -1,38 +1,75 @@
+import sys
+import ipaddress
 import socket
+import os
 import struct
-import argparse
-import logging
 
-# Настройка логирования
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s [SNIFFER] %(levelname)s: %(message)s',
-    handlers=[
-        logging.FileHandler("sniffer.log"),
-        logging.StreamHandler()
-    ]
-)
 
-def ip_header_unpack(packet):
-    # Первые 20 байт — заголовок IPv4
-    ip_header = packet[0:20]
-    iph = struct.unpack('!BBHHHBBH4s4s', ip_header)
+class IP:
+    def __init__(self, buff=None):
+        header = struct.unpack("<BBHHHBBH4s4s", buff)
+        self.ver = header[0] >> 4
+        self.ihl = header[0] & 0xF
+        self.tos = header[1]
+        self.len = header[2]
+        self.id = header[3]
+        self.offset = header[4]
+        self.ttl = header[5]
+        self.protocol_num = header[6]
+        self.sum = header[7]
+        self.src = header[8]
+        self.dst = header[9]
+        # IP-адреса, понятные человеку
+        self.src_address = ipaddress.ip_address(self.src)
+        self.dst_address = ipaddress.ip_address(self.dst)
+        # сопоставляем константы протоколов с их названиями
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except Exception as e:
+            print('%s No protocol for %s' % (e, self.protocol_num))
+            self.protocol = str(self.protocol_num)
 
-    version_ihl = iph[0]
-    version = version_ihl >> 4
-    ihl = version_ihl & 0xF
+def sniff(host):
+    # должно быть знакомо по предыдущему примеру
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
 
-    ttl = iph[5]
-    protocol = iph[6]
-    src_ip = socket.inet_ntoa(iph[8])
-    dst_ip = socket.inet_ntoa(iph[9])
+    sniffer = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_RAW, socket_protocol
+    )
+    sniffer.bind((host, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    try:
+        while True:
+            # читаем пакет
+            raw_buffer = sniffer.recvfrom(65535)[0]
 
-    return {
-        "version": version,
-        "header_length": ihl * 4,
-        "ttl": ttl,
-        "protocol": protocol,
-        "source_ip": src_ip,
-        "destination_ip": dst_ip
-    }
+            # создаём IP-заголовок из первых 20 байтов
+            ip_header = IP(raw_buffer[0:20])
+            # выводим обнаруженные протокол и адреса
+            print(
+                'Protocol: %s %s -> %s' % (
+                    ip_header.protocol,
+                    ip_header.src_address,
+                    ip_header.dst_address
+                )
+            )
+    except KeyboardInterrupt:
+        # если мы в виндоус, то отключаем неизбирательный режим
+        if os.name == 'nt':
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        sys.exit()
 
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        host = '192.168.23.221'
+    sniff(host)
--- decoding_ip_packets/main.py
+++ decoding_ip_packets/main.py
@@ -1,8 +1,69 @@
-import requests
+import struct
+import ipaddress
 
-def fetch():
-    requests.get("https://kali.org")
+class IP:
+    def __init__(self, raw_data):
+        # Распаковываем первые 20 байт IP-заголовка
+        # Формат:
+        # B  - 1 байт (Version + IHL)
+        # B  - 1 байт (TOS)
+        # H  - 2 байта (Total Length)
+        # H  - 2 байта (Identification)
+        # H  - 2 байта (Flags + Fragment Offset)
+        # B  - 1 байт (TTL)
+        # B  - 1 байт (Protocol)
+        # H  - 2 байта (Header checksum)
+        # 4s - 4 байта (Source IP)
+        # 4s - 4 байта (Destination IP)
+        ip_header = struct.unpack('!BBHHHBBH4s4s', raw_data[:20])
 
-for _ in range(10):
-    fetch()
+        # Версия IP - старшие 4 бита первого байта
+        self.version = ip_header[0] >> 4
+        # Длина заголовка (IHL) - младшие 4 бита первого байта, количество 32-битных слов
+        self.ihl = ip_header[0] & 0xF
+        # TOS (Type of Service)
+        self.tos = ip_header[1]
+        # Общая длина пакета (включая заголовок и данные)
+        self.total_length = ip_header[2]
+        # Идентификатор пакета
+        self.identification = ip_header[3]
+        # Флаги и смещение фрагмента (3 бита флагов + 13 битов смещения)
+        self.flags_fragment_offset = ip_header[4]
+        # Время жизни (TTL)
+        self.ttl = ip_header[5]
+        # Протокол (TCP=6, UDP=17, ICMP=1 и др.)
+        self.protocol = ip_header[6]
+        # Контрольная сумма заголовка
+        self.header_checksum = ip_header[7]
+        # IP-адрес источника (4 байта)
+        self.src = ipaddress.ip_address(ip_header[8])
+        # IP-адрес назначения (4 байта)
+        self.dst = ipaddress.ip_address(ip_header[9])
 
+        # Флаги — выделяем 3 старших бита из flags_fragment_offset (16 бит)
+        self.flags = (self.flags_fragment_offset >> 13) & 0x7
+        # Смещение фрагмента — младшие 13 бит
+        self.fragment_offset = self.flags_fragment_offset & 0x1FFF
+
+    def __str__(self):
+        return (f"IP Packet:\n"
+                f" Version: {self.version}\n"
+                f" Header Length: {self.ihl * 4} bytes\n"
+                f" Type of Service: {self.tos}\n"
+                f" Total Length: {self.total_length}\n"
+                f" Identification: {self.identification}\n"
+                f" Flags: {bin(self.flags)}\n"
+                f" Fragment Offset: {self.fragment_offset}\n"
+                f" TTL: {self.ttl}\n"
+                f" Protocol: {self.protocol}\n"
+                f" Header Checksum: {hex(self.header_checksum)}\n"
+                f" Source IP: {self.src}\n"
+                f" Destination IP: {self.dst}")
+
+# Пример использования
+if __name__ == "__main__":
+    # Пример сырых данных IP-пакета (20 байт IP заголовка)
+    # Обычно такие данные получаются из сырого сокета
+    sample_raw_ip = b'\x45\x00\x00\x3c\x1c\x46\x40\x00\x40\x06\xa6\xec\xc0\xa8\x01\x64\xc0\xa8\x01\xc8'
+    ip_packet = IP(sample_raw_ip)
+    print(ip_packet)
